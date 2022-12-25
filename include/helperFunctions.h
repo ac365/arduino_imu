c05bfa3de4811e8f80705d6cd2e95daed4264ee2
Apoorva Chaubal <chaubal.4@osu.edu>
2022-12-24 22:50:50 -0600

    quat2Euler helper function added

--- include/helperFunctions.h
+++ include/helperFunctions.h
@@ -6,7 +6,8 @@ class HelperFunctions
 public:
     float g = 9.80665;
 
-    Matrix<4,1> euler2Quaternion(Matrix<3,1> eulerAngles);
+    Matrix<4> euler2Quaternion(Matrix<3> eul);
+    Matrix<3> quaternion2Euler(Matrix<4> q);
 
 private:
 
--- src/helperFunctions.cpp
+++ src/helperFunctions.cpp
@@ -1,11 +1,11 @@
 #include "helperFunctions.h"
 
-Matrix<4,1> HelperFunctions::euler2Quaternion(Matrix<3,1> eulerAngles)
+Matrix<4> HelperFunctions::euler2Quaternion(Matrix<3> eul)
 {
     //re-package euler angles
-    float phi  =eulerAngles(0,0);
-    float theta=eulerAngles(1,0);
-    float psi  =eulerAngles(2,0);
+    float phi  =eul(0,0);
+    float theta=eul(1,0);
+    float psi  =eul(2,0);
 
     //convert to quaternions
     float cp=cos(0.5*phi);   float sp=sin(0.5*phi);
@@ -19,4 +19,21 @@ Matrix<4,1> HelperFunctions::euler2Quaternion(Matrix<3,1> eulerAngles)
     q(3)=ss*ct*cp - cs*st*sp;
 
     return q;
+}
+
+Matrix<3> HelperFunctions::quaternion2Euler(Matrix<4> q)
+{
+    //re-package quaternions
+    float q0 = q(0);
+    float q1 = q(1);
+    float q2 = q(2);
+    float q3 = q(3);
+
+    //convert to euler angles
+    Matrix<3> eul;
+    eul(0)=atan2(2*(q1*q2 + q0*q3),(q0*q0 + q1*q1 - q2*q2 - q3*q3));//psi
+    eul(1)=asin(-2*(q1*q3 - q0*q2));                                //theta
+    eul(2)=atan2(2*(q2*q3 + q0*q1),q0*q0 - q1*q1 - q2*q2 + q3*q3);  //phi
+    
+    return eul;
 }
